2749a9c5e1dd94c9bd36a0505b81a6aebec34c30
haoyuf1 <haoyuf1@uci.edu>
2022-05-27 10:29:51 -0500

    Modifiying App.js 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+export default {
+  queryDashboard: '/dashboard',
+  queryTableTitles: 'OPTIONS /sv/voyage/',
+  queryVoyageList: 'GET /sv/voyage/',
+  queryFilter: 'POST /sv/voyage/',
+  querySort:'POST /sv/voyage/',
+  queryAutoComplete:'POST /sv/voyage/autocomplete',
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  queryIntegerRange:'GET /sv/voyage/aggregations'
+
 }
-
-export default App;
